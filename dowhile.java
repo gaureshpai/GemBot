c66baf7aa6cbe9bf940300ac9e7e14ca2a2d1bcb
Gauresh G Pai <paigauresh@gmail.com>
2023-11-30 19:29:32 +0530

    code upload

---
+++ dowhile.java
@@ -0,0 +1,10 @@
+package JAVA;
+class dowhile{
+    public static void main(String args[]){
+        int n=10;
+        do{
+            System.out.println("kick"+n);
+            n--;
+        }while(n>0);
+    }
+}
---
+++ while.java
@@ -0,0 +1,10 @@
+package JAVA;
+class while{
+    public static void main(String args[]){
+        int n=10;
+        while(n>0){
+            System.out.println("kick"+n);
+            n--;
+        }
+    }
+}
