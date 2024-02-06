4e9d5f427ea9fedf02c9275f30ea080843d54252
Gauresh G Pai <paigauresh@gmail.com>
2024-02-06 14:50:57 +0530

    :)

--- switch.java
+++ switch.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner;
-class switch{
+class switch1{
     public static void main(String []args){
         Scanner obj = new Scanner(System.in);
         System.out.println("Enter the month:");
