9f4e9eac8305becd01a0ab87025d90d5273a11f1
paigauresh <paigauresh@gmail.com>
2024-03-24 01:55:13 +0530

    Reset!

--- add.java
+++
@@ -1,7 +0,0 @@
-class add{
-    public static void main(String args[]){
-        int a=10,b=15,c;
-        c=a+b;
-        System.out.println("Sum: "+c);
-    }
-}
--- world.java
+++
@@ -1,6 +0,0 @@
-
-class world{
-    public static void main(String args[]){
-        System.out.println("Hello World !");
-    }
-}
